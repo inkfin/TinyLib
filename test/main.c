3c82b2a059f381d41cac42a44c7c7d04357833e2
inkfin <inkfinite@qq.com>
2025-05-05 21:32:25 -0400

    fix(logging): fix logging test cases

--- test/main.c
+++ test/main.c
@@ -6,9 +6,15 @@ int main(void)
     puts("Start testing...");
     puts("=========================");
 
+    // Test common macros
+    common_test_cases();
+
     // Test dynamic array
     dyn_arr_test_cases();
 
     // Test memory pool
     // memory_pool_test_cases();
+
+    // Test logging
+    logging_test_cases();
 }
--- test/test_common.c
+++ test/test_common.c
@@ -4,5 +4,8 @@
 
 void common_test_cases(void)
 {
+    puts("- Common Macros Test Cases");
+    puts("  TL_FOREACH(puts, \"Hello\", \"World!\"): ");
     TL_FOREACH(puts, "Hello", "World!");
+    puts("\n");
 }
