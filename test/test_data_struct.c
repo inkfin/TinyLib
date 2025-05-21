61e754cba80c791e2d4fc333911e6903b379497d
inkfin <inkfinite@qq.com>
2025-05-21 02:03:20 -0400

    update(data_strcut): uppercase type name

--- test/test_data_struct.c
+++ test/test_data_struct.c
@@ -15,7 +15,7 @@ int dyn_arr_test_cases(void)
 
     {
         int v = arrpush(int_arr, 11);
-        tlds_array_header* arr_header = tlds_header(int_arr);
+        TLDS_array_header* arr_header = tlds_header(int_arr);
         assert(v == 11);
         assert(arrlen(int_arr) == 1);
         assert(arrcap(int_arr) == 4);
@@ -46,7 +46,7 @@ int dyn_arr_test_cases(void)
 
     {
         arrappend(int_arr, int_arr2);
-        tlds_array_header* arr_header2 = tlds_header(int_arr2);
+        TLDS_array_header* arr_header2 = tlds_header(int_arr2);
         assert(arrlen(int_arr) == 12);
         assert(arrcap(int_arr) == 16);
     }
