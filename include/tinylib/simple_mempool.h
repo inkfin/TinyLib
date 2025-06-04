31a0bdf3ee2ae6c14a303ec3810601eee0c795a9
inkfin <inkfinite@qq.com>
2025-06-04 15:14:34 -0400

    refactor(rename tiny_mempool to simple_mempool)

--- include/tinylib/simple_mempool.h
+++ include/tinylib/simple_mempool.h
@@ -1,13 +1,13 @@
-#ifndef TINY_MEMPOOL_H
-#define TINY_MEMPOOL_H
+#ifndef SIMPLE_MEMPOOL_H
+#define SIMPLE_MEMPOOL_H
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <stddef.h>
 
-typedef unsigned char byte_t;
+typedef uint8_t byte_t;
 
-#define MEMORY_POOL_SIZE 1024 // Size of the memory pool in bytes
+#define MEMPOOL_TOTAL_SIZE 1024 // Size of the memory pool in bytes
 
 typedef struct memblock {
     struct memblock* next;
@@ -15,30 +15,35 @@ typedef struct memblock {
 
 typedef struct mempool {
     memblock_t*   free_list;
-    byte_t pool[MEMORY_POOL_SIZE];
+    byte_t pool[MEMPOOL_TOTAL_SIZE];
 } mempool_t;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void init_mempool(mempool_t* pool);
+void mp_init(mempool_t* pool);
+void* mp_alloc(mempool_t* pool);
+void mp_free(mempool_t* pool, void* ptr);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif // TINY_MEMPOOL_H
+#endif // SIMPLE_MEMPOOL_H
 
-#define TINY_MEMPOOL_IMPL
 #ifdef TINY_MEMPOOL_IMPL
 
-void init_mempool(mempool_t* pool)
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void mp_init(mempool_t* pool)
 {
     pool->free_list = (memblock_t*)pool->pool;
 
     memblock_t* current = pool->free_list;
-    for (int i = 0; i < (int)(MEMORY_POOL_SIZE / sizeof(memblock_t)) - 1; i++) {
+    for (int i = 0; i < (int)(MEMPOOL_TOTAL_SIZE / sizeof(memblock_t)) - 1; i++) {
         current->next
             = (memblock_t*)((byte_t*)current + sizeof(memblock_t));
         current = current->next;
@@ -47,7 +52,7 @@ void init_mempool(mempool_t* pool)
     current->next = NULL; // Last block points to NULL
 }
 
-void* allocate_memory(mempool_t* pool)
+void* mp_alloc(mempool_t* pool)
 {
     if (pool->free_list == NULL) {
         fprintf(stderr, "Memory pool exhausted\n");
@@ -60,7 +65,7 @@ void* allocate_memory(mempool_t* pool)
     return (void*)block; // Return the allocated block
 }
 
-void free_memory(mempool_t* pool, void* ptr)
+void mp_free(mempool_t* pool, void* ptr)
 {
     if (!ptr)
         return; // Nothing to free
@@ -69,5 +74,9 @@ void free_memory(mempool_t* pool, void* ptr)
     block->next = pool->free_list; // Add the block back to the free list
     pool->free_list = block; // Update the free list pointer
 }
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // TINY_MEMPOOL_IMPL
--- test/test_simple_mempool.c
+++ test/test_simple_mempool.c
@@ -1,32 +1,32 @@
 #define TINY_MEMPOOL_IMPL
-#include "tinylib/tiny_mempool.h"
+#include "tinylib/simple_mempool.h"
 
 int main(void) {
     puts("==========================");
     puts("Tiny Memory Pool Test");
 
     mempool_t pool;
-    init_mempool(&pool);
+    mp_init(&pool);
     puts("Memory pool initialized.");
 
-    void* block1 = allocate_memory(&pool);
+    void* block1 = mp_alloc(&pool);
     if (block1) {
         fprintf(stdout, "Allocated first block: %p; freelist=%p\n", block1, pool.free_list);
     } else {
         puts("Failed to allocate first block.");
     }
 
-    void* block2 = allocate_memory(&pool);
+    void* block2 = mp_alloc(&pool);
     if (block2) {
         fprintf(stdout, "Allocated second block: %p; freelist=%p\n", block2, pool.free_list);
     } else {
         puts("Failed to allocate second block.");
     }
 
-    free_memory(&pool, block1);
+    mp_free(&pool, block1);
     fprintf(stdout, "Freed first block; free_list=%p\n", pool.free_list);
 
-    free_memory(&pool, block2);
+    mp_free(&pool, block2);
     fprintf(stdout, "Freed second block; free_list=%p\n", pool.free_list);
 
     puts("Memory pool test completed.");
